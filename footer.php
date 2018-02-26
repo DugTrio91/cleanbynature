0846803d99c44f3eb839bb542ef43eebe014e5ee
Mark Allen <markdallen@me.com>
2018-02-26 20:02:00 +0000

    move header and footer into included php files

---
+++ footer.php
@@ -0,0 +1,18 @@
+</body>
+<footer>
+    <div class="footerlogo footercontainer"></div>
+    <div class="footeraddress footercontainer">
+        <p>34 Ravendale Road</p>
+        <p>Darlington</p>
+        <p>DL3 8EA</p>
+    </div>
+    <div class="footercontact footercontainer">
+        <p><a href="mailto:callcleanbynature@gmail.com">callcleanbynature@gmail.com</a></p>
+        <p><a href="tel:07714346060"><i class="fa fa-phone"></i> 07714 346 060</a></p>
+    </div>
+    <div class="footersocial footercontainer">
+        <a href="#"><i class="fa fa-facebook-square fa-4x"></i></a>
+        <a href="#"><i class="fa fa-twitter-square fa-4x"></i></a>
+    </div>
+</footer>
+</html>
