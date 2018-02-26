ab7b49da45a2992b2f88ecd8ec10b4c7617a555c
Mark Allen <markdallen@me.com>
2018-02-26 20:04:27 +0000

    create seperate php files for nav and banner

---
+++ banner.php
@@ -0,0 +1,22 @@
+<div class="feature-banner">
+    <div class="images">
+        <img src="images/cbnpotential/stock-photo-cleaning-concept-with-supplies-on-wooden-background-view-from-above-380743105.jpg" alt="">
+        <img src="images/cbnpotential/stock-photo-woman-who-cleans-the-floor-of-the-house-103819709.jpg" alt="">
+        <img src="images/cbnpotential/stock-photo-woman-cleaning-the-counter-in-the-kitchen-117715075.jpg" alt="">
+    </div>
+    <div class="statement">
+        <div class="quotes">
+            <h2 style="color:#fff"><strong>Regular Cleaning</strong></h2>
+            <p style="color:#fff">Bogged down with those time consuming household cleaning chores?</p>
+            <p style="color:#fff">You can take control by booking a weekly, fortnightly or monthly home clean.</p>
+        </div>
+        <div class="quotes">
+            <h2 style="color:#fff"><strong>Office &amp; Showroom Cleaning</strong></h2>
+            <p style="color:#fff">We look after your working environment maintaining those front of house first impressions and a hygienic workplace for staff.</p>
+        </div>
+        <div class="quotes">
+            <h2 style="color:#fff"><strong>End of Tenancy Cleaning</strong></h2>
+            <p style="color:#fff">Don't lose your deposit or let it be withheld. We offer a full and detailed clean of a variety of properties.</p>
+        </div>
+    </div>
+</div>
